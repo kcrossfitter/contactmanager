1c2276a088d7ee82d7b2efbdaf7a4714b629936e
Sangwook Cho <korean.crossfitter@gmail.com>
2018-12-02 23:30:30 +0900

    Updates

---
+++ precache-manifest.31b32319f73d7ae3754a5aba2ebf5690.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "72aa82900d090390d8dc",
+    "url": "/contactmanager/static/js/runtime~main.72aa8290.js"
+  },
+  {
+    "revision": "81e2cac9aba001e983ce",
+    "url": "/contactmanager/static/js/main.81e2cac9.chunk.js"
+  },
+  {
+    "revision": "9844826478c027ddf2f4",
+    "url": "/contactmanager/static/js/1.98448264.chunk.js"
+  },
+  {
+    "revision": "81e2cac9aba001e983ce",
+    "url": "/contactmanager/static/css/main.da3b6268.chunk.css"
+  },
+  {
+    "revision": "266219f1784859122b94ff55115c2313",
+    "url": "/contactmanager/index.html"
+  }
+];
